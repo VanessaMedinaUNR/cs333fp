97f48d9459efd54e5411a21fba84c33e1c24cd6d
Vanessa <vanessamedina2281@gmail.com>
2025-05-05 16:56:29 -0700

    start todo

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 # Create an instance of the Flask class
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed= db.Column(db.Integer, default =0)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 # Define a route for the home page
 @app.route('/')
