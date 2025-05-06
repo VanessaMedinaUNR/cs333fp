0148f93c7dc23f83daef4b994b06e69dba10659f
Vanessa <vanessamedina2281@gmail.com>
2025-05-05 17:18:09 -0700

    put date created

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Create an instance of the Flask class
 app = Flask(__name__)
@@ -10,6 +11,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed= db.Column(db.Integer, default =0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
