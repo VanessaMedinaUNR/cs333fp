7f757b60a1fae10b5ad618c293f239de7f7a5a6f
Vanessa <vanessamedina2281@gmail.com>
2025-05-05 18:13:16 -0700

    fixed timezone error

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timezone
 
 # Create an instance of the Flask class
 app = Flask(__name__)
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.time())
+    date_created = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
 
     def __repr__(self):
         return '<Task %r>' % self.id
